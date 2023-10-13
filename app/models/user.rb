f419af215cfe582d2fcd1772b31d168915774662
Alejandro Piñango <alejandropinango@gmail.com>
2023-10-13 19:02:07 -0300

    Se modificaron registration y application controller y se agregó el enum de los roles.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Enum de roles
+  enum role: {  user: 'client',
+                admin: 'admin',
+                administrative: 'administrative',
+  }, _default: 'client'
+  
+  # Validaciones de campos
+  validates :username, presence: { message: "Username field cannot be blank" }, uniqueness: { message: "Username is already in use" }
+  validates :role, presence: { message: "Role field cannot be blank" }
+  validates :nombre, presence: { message: "Nombre field cannot be blank" }
+  validates :apellido, presence: { message: "Apellido field cannot be blank" }
+  validates :telefono, presence: { message: "Telefono field cannot be blank" }
+  validates :direccion, presence: { message: "Direccion field cannot be blank" }
+  
+
 end
